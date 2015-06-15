78a95315a983e0d1ef4c14f5e06db29e5ce52b97
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-06-15 15:11:55 +0000

    + BGU-112 ORG: Setup united project space for Significant ownership : ...

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/EnumsLister.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BGU.DRPL.SignificantOwnership.Utility;
+using BGU.DRPL.SignificantOwnership.Core.Spares2;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares
+{
+    public class EnumsLister
+    {
+        #region cctor(s)
+        private EnumsLister()
+        { }
+
+        private static EnumsLister _instance;
+        public static EnumsLister Instance
+        {
+            get 
+            {
+                if (_instance == null) _instance = new EnumsLister(); return _instance;
+            }
+        }
+        #endregion
+        #region method(s)
+        public List<EnumType> ListEntityType() { return EnumType.GetEnumList(typeof(EntityType)); }
+        public List<EnumType> ListOwnershipType() { return EnumType.GetEnumList(typeof(OwnershipType)); }
+        public List<EnumType> ListSexType() { return EnumType.GetEnumList(typeof(SexType)); }
+        public List<EnumType> ListEmploymentState() { return EnumType.GetEnumList(typeof(EmploymentState)); }
+        public List<EnumType> ListEmploymentTerminationType() { return EnumType.GetEnumList(typeof(EmploymentTerminationType)); }
+        public List<EnumType> ListHigherEducationDegreeType() { return EnumType.GetEnumList(typeof(HigherEducationDegreeType)); }
+        public List<EnumType> ListDegreeHonourType() { return EnumType.GetEnumList(typeof(DegreeHonourType)); }
+        public List<EnumType> ListFundsOriginType() { return EnumType.GetEnumList(typeof(FundsOriginType)); }
+        public List<EnumType> ListPaymentType() { return EnumType.GetEnumList(typeof(PaymentType)); }
+        public List<EnumType> ListFinancialGuarantorRoleType() { return EnumType.GetEnumList(typeof(FinancialGuarantorRoleType)); }
+        public List<EnumType> ListBreachOfLawType() { return EnumType.GetEnumList(typeof(BreachOfLawType)); }
+        public List<EnumType> ListSentenceType() { return EnumType.GetEnumList(typeof(SentenceType)); }
+        public List<EnumType> ListTypicalApplicationAttachement() { return EnumType.GetEnumList(typeof(TypicalApplicationAttachement)); }
+        public List<EnumType> ListFinancialServicesType() { return EnumType.GetEnumList(typeof(FinancialServicesType)); }
+        public List<EnumType> ListBankingActivityType() { return EnumType.GetEnumList(typeof(BankingActivityType)); }
+        public List<EnumType> ListAssociatedPersonRole() { return EnumType.GetEnumList(typeof(AssociatedPersonRole)); }
+        public List<EnumType> ListManagementPosition() { return EnumType.GetEnumList(typeof(ManagementPosition)); }
+        public List<EnumType> ListInsolvencyStatus() { return EnumType.GetEnumList(typeof(InsolvencyStatus)); }
+        public List<EnumType> ListWellKnownCreditRatingAgencyType() { return EnumType.GetEnumList(typeof(WellKnownCreditRatingAgencyType)); }
+        public List<EnumType> ListLongTermCreditRatingType() { return EnumType.GetEnumList(typeof(LongTermCreditRatingType)); }
+        public List<EnumType> ListShortTermCreditRatingType() { return EnumType.GetEnumList(typeof(ShortTermCreditRatingType)); }
+        public List<EnumType> ListBankruptcyCaseResolutionType() { return EnumType.GetEnumList(typeof(BankruptcyCaseResolutionType)); }
+        public List<EnumType> ListCourtInstanceType() { return EnumType.GetEnumList(typeof(CourtInstanceType)); }
+        public List<EnumType> ListCourtDecisionType() { return EnumType.GetEnumList(typeof(CourtDecisionType)); }
+        public List<EnumType> ListBankAssociatedPeronsCode315p() { return EnumType.GetEnumList(typeof(BankAssociatedPeronsCode315p)); }
+
+        //Spares2 enums
+        public List<EnumType> ListFinancialInstitutionType() { return EnumType.GetEnumList(typeof(FinancialInstitutionType)); }
+        public List<EnumType> ListFinancialInstitutionStatus() { return EnumType.GetEnumList(typeof(FinancialInstitutionStatus)); }
+        public List<EnumType> ListCompanyOwnershipType() { return EnumType.GetEnumList(typeof(CompanyOwnershipType)); }
+        public List<EnumType> ListInstitutionLevel() { return EnumType.GetEnumList(typeof(InstitutionLevel)); }
+        #endregion
+
+        #region prop(s)
+        public static List<EnumType> EntityTypeList { get { return EnumType.GetEnumList(typeof(EntityType)); } }
+        public static List<EnumType> OwnershipTypeList { get { return EnumType.GetEnumList(typeof(OwnershipType)); } }
+        public static List<EnumType> SexTypeList { get { return EnumType.GetEnumList(typeof(SexType)); } }
+        public static List<EnumType> EmploymentStateList { get { return EnumType.GetEnumList(typeof(EmploymentState)); } }
+        public static List<EnumType> EmploymentTerminationTypeList { get { return EnumType.GetEnumList(typeof(EmploymentTerminationType)); } }
+        public static List<EnumType> HigherEducationDegreeTypeList { get { return EnumType.GetEnumList(typeof(HigherEducationDegreeType)); } }
+        public static List<EnumType> DegreeHonourTypeList { get { return EnumType.GetEnumList(typeof(DegreeHonourType)); } }
+        public static List<EnumType> FundsOriginTypeList { get { return EnumType.GetEnumList(typeof(FundsOriginType)); } }
+        public static List<EnumType> PaymentTypeList { get { return EnumType.GetEnumList(typeof(PaymentType)); } }
+        public static List<EnumType> FinancialGuarantorRoleTypeList { get { return EnumType.GetEnumList(typeof(FinancialGuarantorRoleType)); } }
+        public static List<EnumType> BreachOfLawTypeList { get { return EnumType.GetEnumList(typeof(BreachOfLawType)); } }
+        public static List<EnumType> SentenceTypeList { get { return EnumType.GetEnumList(typeof(SentenceType)); } }
+        public static List<EnumType> TypicalApplicationAttachementList { get { return EnumType.GetEnumList(typeof(TypicalApplicationAttachement)); } }
+        public static List<EnumType> FinancialServicesTypeList { get { return EnumType.GetEnumList(typeof(FinancialServicesType)); } }
+        public static List<EnumType> BankingActivityTypeList { get { return EnumType.GetEnumList(typeof(BankingActivityType)); } }
+        public static List<EnumType> AssociatedPersonRoleList { get { return EnumType.GetEnumList(typeof(AssociatedPersonRole)); } }
+        public static List<EnumType> ManagementPositionList { get { return EnumType.GetEnumList(typeof(ManagementPosition)); } }
+        public static List<EnumType> InsolvencyStatusList { get { return EnumType.GetEnumList(typeof(InsolvencyStatus)); } }
+        public static List<EnumType> WellKnownCreditRatingAgencyTypeList { get { return EnumType.GetEnumList(typeof(WellKnownCreditRatingAgencyType)); } }
+        public static List<EnumType> LongTermCreditRatingTypeList { get { return EnumType.GetEnumList(typeof(LongTermCreditRatingType)); } }
+        public static List<EnumType> ShortTermCreditRatingTypeList { get { return EnumType.GetEnumList(typeof(ShortTermCreditRatingType)); } }
+        public static List<EnumType> BankruptcyCaseResolutionTypeList { get { return EnumType.GetEnumList(typeof(BankruptcyCaseResolutionType)); } }
+        public static List<EnumType> CourtInstanceTypeList { get { return EnumType.GetEnumList(typeof(CourtInstanceType)); } }
+        public static List<EnumType> CourtDecisionTypeList { get { return EnumType.GetEnumList(typeof(CourtDecisionType)); } }
+        public static List<EnumType> BankAssociatedPeronsCode315pList { get { return EnumType.GetEnumList(typeof(BankAssociatedPeronsCode315p)); } }
+        public static List<EnumType> FinancialInstitutionTypeList { get { return EnumType.GetEnumList(typeof(FinancialInstitutionType)); } }
+        public static List<EnumType> FinancialInstitutionStatusList { get { return EnumType.GetEnumList(typeof(FinancialInstitutionStatus)); } }
+        public static List<EnumType> CompanyOwnershipTypeList { get { return EnumType.GetEnumList(typeof(CompanyOwnershipType)); } }
+        public static List<EnumType> InstitutionLevelList { get { return EnumType.GetEnumList(typeof(InstitutionLevel)); } }
+        #endregion
+    }
+}
