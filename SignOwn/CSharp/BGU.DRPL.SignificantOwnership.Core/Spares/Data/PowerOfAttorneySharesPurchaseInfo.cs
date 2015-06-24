44cc77c2517a8e1457ed2f76de50da343d5f3200
jefe <vmdrot@users.noreply.github.com>
2015-06-24 10:03:01 +0000

    +*- BGUSO-72 Зміни в рамках 357 - Додаток 2 : забуті "запчастини" - розділ II п. 6 +*- BGUSO-57 RegNLic: АНКЕТА юридичної особи (у тому числі банку) (Додаток 2) : відображено зміни 357 пост.

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/PowerOfAttorneySharesPurchaseInfo.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/PowerOfAttorneySharesPurchaseInfo.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
 {
+    /// <summary>
+    /// Інформація про намір щодо набуття опосередкованої істотної участі в банку за довіреністю
+    /// </summary>
     [System.ComponentModel.Editor(typeof(BGU.DRPL.SignificantOwnership.Core.TypeEditors.PowerOfAttorneySharesPurchaseInfo_Editor), typeof(System.Drawing.Design.UITypeEditor))]
     public class PowerOfAttorneySharesPurchaseInfo
     {
+        [DisplayName("Реквізити довіреності")]
+        [Description("Особа, яка видаватиме довіреність, Запланований термін дії довіреності, Інша інформація")]
         public PowerOfAttorneyInfo PowerOfAttorney { get; set; }
+
+        [DisplayName("% участі довірителя")]
+        [Description("Розмір участі довірителя в банку, %")]
         public decimal SharesPct { get; set; }
+        
+        [DisplayName("Кількість голосів")]
+        [Description("Кількість голосів у банку, щодо яких передається право голосу")]
+        public int Votes { get; set; }
+        
+        [DisplayName("% голосів")]
+        [Description("Відсоток голосів у банку, щодо яких передається право голосу")]
         public decimal VotesPct { get; set; }
     }
 }
