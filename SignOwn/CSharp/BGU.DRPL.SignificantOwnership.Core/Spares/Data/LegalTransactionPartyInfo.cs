ddddb603b8e79103b8776d531e8d10ad3366c248
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-06-25 12:06:02 +0000

    + BGUSO-76 Розгорнута структура для сторони правочину - DONE

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/LegalTransactionPartyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
+{
+    /// <summary>
+    /// Інформація про сторону правочину
+    /// </summary>
+    public class LegalTransactionPartyInfo
+    {
+        [DisplayName("Сторона")]
+        [Description("Ідентифікація сторони правочину")]
+        public GenericPersonID Party { get; set; }
+        [DisplayName("Роль")]
+        [Description("Роль сторони у правочині")]
+        public LegalTransactionPartyRoleType Role { get; set; }
+        [DisplayName("Роль (інше)")]
+        [Description("Роль сторони у правочині, як в полі \"Роль\" вказано інше")]
+        public string RoleIfOther { get; set; }
+    }
+}
