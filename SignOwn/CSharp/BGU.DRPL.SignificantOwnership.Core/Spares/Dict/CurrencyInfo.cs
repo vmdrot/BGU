09fc6b972f3dd48ad30994ae30a87aadba739c2c
jefe <vmdrot@users.noreply.github.com>
2015-06-26 13:33:55 +0000

    +* BGUSO-72 Зміни в рамках 357 - Додаток 2 : переміщено поле до більш релевантного класу +* BGUSO-77 WPFGen: Combo's : in progress

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Dict/CurrencyInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares.Dict
+{
+    public class CurrencyInfo
+    {
+        /// <summary>
+        /// ENTITY за стандартом ISO
+        /// </summary>
+        public string CountryNm { get; set; }
+
+        /// <summary>
+        /// ENTITY за стандартом ISO
+        /// </summary>
+        public string CCYName { get; set; }
+
+        /// <summary>
+        /// ENTITY за стандартом ISO
+        /// </summary>
+        public string CCYCode { get; set; }
+
+        /// <summary>
+        /// ENTITY за стандартом ISO
+        /// </summary>
+        public string CCYCode3D { get; set; }
+
+        /// <summary>
+        /// ENTITY за стандартом ISO
+        /// </summary>
+        public int? CCYMinorUnit { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <seealso cref="http://www.iso.org/iso/en-US/home/standards/currency_codes.htm"/>
+        public static List<CurrencyInfo> AllCurrencies
+        {
+            get
+            {
+                return new List<CurrencyInfo>(); //todo
+            }
+        }
+    }
+}
