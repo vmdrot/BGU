e893a3d1f12491ab5e42149155d18ab08528da27
jefe <vmdrot@users.noreply.github.com>
2015-06-30 08:25:21 +0000

    + BGUSO-78 WPF: combo binding as selectedItem doesn't bind back : logging

---
+++ SignOwn/CSharp/Evolvex.Utility.Core/Common/ILog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.Utility.Core.Common
+{
+    public interface ILog
+    {
+        void Debug(string message, params object[] args);
+        void Debug(string message);
+        void Error(string message, params object[] args);
+        void Error(string message);
+        void Fatal(string message, params object[] args);
+        void Fatal(string message);
+        void Info(string message, params object[] args);
+        void Info(string message);
+        void Warn(string message);
+        void Warn(string message, params object[] args);
+    }
+}
