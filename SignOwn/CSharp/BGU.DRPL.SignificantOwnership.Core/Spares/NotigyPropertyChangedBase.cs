1f6836a6c6bdd70140d241fc93b3b10d5d8716d5
jefe <vmdrot@users.noreply.github.com>
2015-07-02 13:31:47 +0000

    +* BGUSO-91 WPFGen: Conditional fields visibility : framework implemented & caveats detected, along with heals

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/NotigyPropertyChangedBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Spares
+{
+    public class NotigyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+    }
+}
---
+++ SignOwn/CSharp/Evolvex.Utility.Core/ComponentModelEx/UIConditionalVisibilityAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.Utility.Core.ComponentModelEx
+{
+    public class UIConditionalVisibilityAttribute : Attribute
+    {
+
+        public string VisibilityMemberPath { get; set; }
+
+        public UIConditionalVisibilityAttribute() : this(string.Empty) { }
+        public UIConditionalVisibilityAttribute(string visibilityMemberPath) : base()
+        {
+            VisibilityMemberPath = visibilityMemberPath;
+        }
+    }
+}
