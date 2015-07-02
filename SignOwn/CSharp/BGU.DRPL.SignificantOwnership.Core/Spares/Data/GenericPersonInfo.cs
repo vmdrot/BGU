e9dc7c8921b4cb1efba53f1e752eeee2dc90ca02
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-07-02 16:22:05 +0000

    +* BGUSO-92 WPFGen: Radio groups instead of combos : DONE

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/GenericPersonInfo.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/GenericPersonInfo.cs
@@ -14,7 +14,7 @@ namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
     [System.ComponentModel.Editor(typeof(BGU.DRPL.SignificantOwnership.Core.TypeEditors.GenericPersonInfo_Editor), typeof(System.Drawing.Design.UITypeEditor))]
     public class GenericPersonInfo : NotigyPropertyChangedBase
     {
-        private EntityType _PersonType;
+        private EntityType _PersonType = EntityType.Physical;
 
         [DisplayName("Тип особи")]
         [Description("Тип особи")]
---
+++ SignOwn/CSharp/Evolvex.Utility.Core/ComponentModelEx/UIUsageRadioButtonGroupAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.Utility.Core.ComponentModelEx
+{
+    public class UIUsageRadioButtonGroupAttribute : Attribute
+    {
+        public Orientation GroupOrientation { get; set; }
+        public bool ShowNoneItem { get; set; }
+
+        public UIUsageRadioButtonGroupAttribute() : this(Orientation.Vertical, true) { }
+        public UIUsageRadioButtonGroupAttribute(Orientation groupOrientation, bool showNoneItem)
+            : base()
+        {
+            GroupOrientation = groupOrientation;
+            ShowNoneItem = showNoneItem;
+        }
+        
+    }
+}
