246aa438f111033f4926b7521ec0ad86bcc4a5ac
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-07-07 07:26:58 +0000

    +* BGUSO-100 WPFGen: Currency text box dynamic minor unit binding : researched, attempted, failed (so far)

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/CurrencyAmount.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Spares/Data/CurrencyAmount.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 using Evolvex.Utility.Core.ComponentModelEx;
+using BGU.DRPL.SignificantOwnership.Core.Spares.Dict;
+using System.Xml.Serialization;
 
 namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
 {
@@ -12,13 +14,14 @@ namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
     /// </summary>
     [System.ComponentModel.Editor(typeof(BGU.DRPL.SignificantOwnership.Core.TypeEditors.CurrencyAmount_Editor), typeof(System.Drawing.Design.UITypeEditor))]
     [XamlExpanderWrapping(false)]
-    public class CurrencyAmount
+    public class CurrencyAmount : NotifyPropertyChangedBase
     {
         public CurrencyAmount()
         {
             this.CCY = "UAH";
         }
 
+        private string _CCY;
         /// <summary>
         /// Значення за змовчанням - UAH
         /// http://www.iso.org/iso/en-US/home/standards/currency_codes.htm
@@ -28,12 +31,47 @@ namespace BGU.DRPL.SignificantOwnership.Core.Spares.Data
         [DisplayName("Валюта")]
         [Required]
         [UIUsageCombo(ItemsGetterClass=typeof(BGU.DRPL.SignificantOwnership.Core.Spares.Dict.CurrencyInfo), ItemsGetterMemberPath="AllCurrencies", ValueMemberUsageMode=ComboUIValueUsageMode.ValueProperty, ValueMember="CCYCode", DisplayMember="CCYCode", Width="75")]
-        public string CCY { get; set; }
+        public string CCY { get { return _CCY; } set { _CCY = value; OnPropertyChanged("CCY"); OnPropertyChanged("CurrencyFormat"); OnPropertyChanged("CurrencyToolTip"); } }
 
+        private decimal _Amt;
         [Description("Сума")]
         [DisplayName("Сума")]
         [Required]
-        public decimal Amt { get; set; }
+        public decimal Amt { get { return _Amt; } set { _Amt = value; OnPropertyChanged("Amt"); } }
+
+
+        [Browsable(false)]
+        [XmlIgnore]
+        public string CurrencyFormat
+        {
+            get 
+            {
+                CurrencyInfo cci = CurrencyInfo.GetCurrency(CCY);
+                int afterComma = 2;
+                if (cci != null)
+                {
+                    if (cci.CCYMinorUnit != null)
+                        afterComma = (int)cci.CCYMinorUnit;
+                    else afterComma = 0;
+                }
+                return string.Format("{{0:N{0}}}", afterComma);
+            }
+        }
+
+        [Browsable(false)]
+        [XmlIgnore]
+        public string CurrencyToolTip
+        {
+            get
+            {
+                CurrencyInfo cci = CurrencyInfo.GetCurrency(CCY);
+                string rslt = string.Empty;
+                if (cci != null)
+                    rslt = cci.CCYName;
+                return rslt;
+            }
+        }
+
 
         public override string ToString()
         {
