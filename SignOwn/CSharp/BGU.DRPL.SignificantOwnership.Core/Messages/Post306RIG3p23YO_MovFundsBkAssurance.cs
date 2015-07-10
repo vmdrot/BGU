2d08c570d9e1a9abd7ba540a47f58e0c8f32df40
jefe <vmdrot@users.noreply.github.com>
2015-07-10 15:03:34 +0000

    + BGUSO-116 306:Дод.4: П.28: Довідки банків про рух коштів etc. : stubs, started + BGUSO-114 306:Дод2: П.23: Довідки банків про рух коштів etc. : stubs, started

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Messages/Post306RIG3p23YO_MovFundsBkAssurance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Messages
+{
+    class Post306RIG3p23YO_MovFundsBkAssurance
+    {
+    }
+}
