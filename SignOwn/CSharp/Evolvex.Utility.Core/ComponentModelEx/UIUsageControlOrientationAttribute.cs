27736fe8a201832d74e58e91dd0d0f2803c3b7c0
jefe <vmdrot@users.noreply.github.com>
2015-07-12 14:56:26 +0000

    +* BGUSO-119 WPFGen: container orientation : DONE

---
+++ SignOwn/CSharp/Evolvex.Utility.Core/ComponentModelEx/UIUsageControlOrientationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolvex.Utility.Core.ComponentModelEx
+{
+    public class UIUsageControlOrientationAttribute : Attribute
+    {
+
+        public Orientation ContainerOrientation { get; set; }
+        public UIUsageControlOrientationAttribute() :this(Orientation.Vertical)
+        {}
+
+        public UIUsageControlOrientationAttribute(Orientation contOrientation)
+            : base()
+        {
+            this.ContainerOrientation = contOrientation;
+        }
+
+
+    }
+}
