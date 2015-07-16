7d7f609b53bf9acdffa42bd69a08224904bc1f15
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-07-16 16:46:06 +0000

    + BKINF-21 ТЗ до змін у Банк-Інфо : started

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Messages/BankInfo/BankInfoEnums.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+namespace BGU.DRPL.SignificantOwnership.Core.Messages.BankInfo
+{ 
+    public enum BankInfoMessageField
+    {
+        [Description("Не вказано")]
+        None = 0,
+        [Description("Назва (254 символів)")]
+        NameFull,
+        [Description("Скорочена назва (80 символів)")]
+        NameShort,
+        [Description("Назва для друку (40 символів)")]
+        NamePrint,
+        [Description("Назва для СЕП (38 символів)")]
+        NameSEP,
+        [Description("Назва для СТАТЗВІТНОСТІ (27 символів)")]
+        NameStats,
+        [Description("реєстраційний номер")]
+        RegNr,
+        [Description("внутрішньобанківський номер")]
+        InternalNr,
+        [Description("номер підрозділу за внутрішньобанківським номером")]
+        BranchNrInternal,
+        [Description("дата відкриття")]
+        OpenDate,
+        [Description("дата внесення до Державного реєстру банків")]
+        RegisteredDate,
+        [Description("область або код області")]
+        Oblast,
+        [Description("назва району")]
+        Raion,
+        [Description("тип та назва населеного пункту")]
+        Town,
+        [Description("адреса")]
+        Address,
+        [Description("поштовий індекс")]
+        ZipCode,
+        [Description("код телефонного зв’язку")]
+        DialCode,
+        [Description("телефон(-и)")]
+        Phone,
+        [Description("факс(-и)")]
+        Fax,
+        [Description("e-mail(-и)")]
+        Email,
+        [Description("веб-сайт")]
+        www,
+        [Description("посада")]
+        MgrPosition,
+        [Description("прізвище")]
+        MgrSurname,
+        [Description("ім’я")]
+        MgrName,
+        [Description("по-батькові")]
+        MgrPatronymic,
+        [Description("номер платіжного документа")]
+        PayDocNr,
+        [Description("дата здійснення оплати")]
+        PayDocDate,
+        [Description("дата внесення")]
+        InputDate,
+        [Description("короткий опис змін")]
+        ShortChangesLog,
+        [Description("Стан")]
+        Status,
+        [Description("Дата закриття")]
+        ClosedDate,
+        [Description("Дата виключення з Державного реєстру банків")]
+        UnregisteredDate,
+        [Description("Перелік операцій")]
+        OperationsListing,
+        [Description("Рішення, на підставі якого вносяться зміни")]
+        Attachement_Resolution,
+    }
+}
---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Messages/BankInfo/BkInfoChangesMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Messages.BankInfo
+{
+    public class BkInfoChangesMessage
+    {
+
+    }
+}
