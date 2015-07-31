da69620121a7d1cfa38a850a4a2637f9133aab25
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-07-31 14:26:03 +0000

    +* BKINF-21 ТЗ до змін у Банк-Інфо : прототип UI (in progress)

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/EKDRBU/Spares/TimeSpanInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Evolvex.Utility.Core.ComponentModelEx;
+
+namespace BGU.DRPL.SignificantOwnership.Core.EKDRBU.Spares
+{
+    [XamlExpanderWrapping(false)]
+    [UIUsageSuppressControlLabels(false)]
+    [UIUsageControlOrientation(Orientation.Horizontal)]
+    public class TimeSpanInfo
+    {
+        [DisplayName("р.")]
+        [Description("(років)")]
+        public int Years { get; set; }
+
+        [DisplayName("м.")]
+        [Description("(місяців)")]
+        public int Months { get; set; }
+        
+        [DisplayName("д.")]
+        [Description("(днів)")]
+        public int Days { get; set; }
+    }
+}
