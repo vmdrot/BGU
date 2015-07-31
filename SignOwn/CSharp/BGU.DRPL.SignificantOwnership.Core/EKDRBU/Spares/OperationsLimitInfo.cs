514e10f318f85c58bd79016211fe621aa08ebd51
jefe <vmdrot@users.noreply.github.com>
2015-07-31 14:26:03 +0000

    +* BKINF-21 ТЗ до змін у Банк-Інфо : прототип UI (in progress)

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/EKDRBU/Spares/OperationsLimitInfo.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/EKDRBU/Spares/OperationsLimitInfo.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using BGU.DRPL.SignificantOwnership.Core.Spares;
 using BGU.DRPL.SignificantOwnership.Core.Spares.Data;
+using System.ComponentModel;
 
 namespace BGU.DRPL.SignificantOwnership.Core.EKDRBU.Spares
 {
     public class OperationsLimitInfo
     {
+        [DisplayName("Тип")]
+        [Description("Тип ліміту")]
         public BankOperationLimitType LimitType { get; set; }
-        public TimeSpan TimeSpecs { get; set; }
-        public CurrencyAmount AmountSpecs {get; set;}
+
+        [DisplayName("Часові ознаки")]
+        [Description("Період, що характеризує ліміт (денний, місячний, річний, інше")]
+        public TimeSpanInfo TimeSpecs { get; set; }
+
+        [DisplayName("Розмір")]
+        [Description("Розміри ліміту (в грошах)")]
+        public CurrencyAmount AmountSpecs { get; set; }
     }
 }
