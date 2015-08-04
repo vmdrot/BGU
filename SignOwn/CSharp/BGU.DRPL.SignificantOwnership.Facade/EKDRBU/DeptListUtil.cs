7e04d24348688d91d74d64f4ea8f8339986b03a0
jefe <vmdrot@users.noreply.github.com>
2015-08-04 12:28:52 +0000

    +* BKINF-21 ТЗ до змін у Банк-Інфо : in progress + BKINF-24 Ієрархія відділень : in progress

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Facade/EKDRBU/DeptListUtil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using BGU.DRPL.SignificantOwnership.Core.EKDRBU;
+using BGU.DRPL.SignificantOwnership.Core.EKDRBU.Legacy;
+
+namespace BGU.DRPL.SignificantOwnership.Facade.EKDRBU
+{
+    public class DeptListUtil
+    {
+        public static void BuildHierarchy(List<DeptListEntry> depts)
+        {
+            var topBanks = depts.Where(dle => dle.TP == "0");
+            var midBranches = depts.Where(dle => dle.TP == "1");
+            var lowBranches = depts.Where(dle => dle.TP == "2");
+            foreach (DeptListEntry dept in depts)
+            {
+                dept.SetHierarchySource(depts);
+                switch (dept.TP)
+                {
+                    case "0":
+                        dept.ParentCode = string.Empty;
+                        break;
+                    case "1": 
+                        dept.ParentCode = topBanks.First(dle => dle.NKB == dept.NKB).DEPCODE;
+                        {
+                            if (topBanks.Any(dle => dle.NKB == dept.NKB))
+                                dept.ParentCode = topBanks.First(dle => dle.NKB == dept.NKB).DEPCODE;
+                        }
+                        break;
+                    case "2":
+                        {
+                            if(midBranches.Any(dle => dle.NKB == dept.NKB && dle.KOB == dept.KOB && dle.NF == dept.NF))
+                                dept.ParentCode = midBranches.First(dle => dle.NKB == dept.NKB && dle.KOB == dept.KOB && dle.NF == dept.NF).DEPCODE;
+                        }
+                        break;
+                    case "3": break;
+                    case "4": break;
+                    case "5": break;
+                    case "6": break;
+                    case "7": break;
+                    default: break;
+                }
+            }
+        }
+    }
+}
---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tests/EKDRBU/BankInfoTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using BGU.DRPL.SignificantOwnership.Utility;
+using BGU.DRPL.SignificantOwnership.Core.EKDRBU.Legacy;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace BGU.DRPL.SignificantOwnership.Tests.EKDRBU
+{
+    [TestFixture]
+    public class BankInfoTests
+    {
+        [Test]
+        public void BuildHierTest()
+        {
+            List<DeptListEntry> depts = ListDepts();
+
+            BGU.DRPL.SignificantOwnership.Facade.EKDRBU.DeptListUtil.BuildHierarchy(depts);
+            PrintDeptsWorker(depts);
+        }
+
+        [Test]
+        public void ParseDeptList()
+        {
+            List<DeptListEntry> depts = ListDepts();
+            PrintDeptsWorker(depts);
+        }
+
+        private void PrintDeptsWorker(List<DeptListEntry> depts)
+        {
+            JsonSerializerSettings settings = new JsonSerializerSettings();
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            foreach (DeptListEntry dle in depts)
+            {
+                string jsonStr = JsonConvert.SerializeObject(dle, settings);
+                Console.WriteLine("{0}", jsonStr);
+            }
+        }
+
+        private List<DeptListEntry> ListDepts()
+        {
+            List<DeptListEntry> depts = new List<DeptListEntry>();
+            DataTable dt = RcuKruReader.Read(@"D:\home\vmdrot\BGU\Var\DerzhReiestr\ShBO\dptlist.dbf");
+            foreach (DataRow dr in dt.Rows)
+            {
+                depts.Add(DeptListEntry.Parse(dr));
+            }
+            return depts;
+        }
+    }
+}
