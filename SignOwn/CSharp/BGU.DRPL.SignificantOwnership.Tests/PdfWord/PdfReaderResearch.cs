5ca7a97bf3dd9f4521acda6c9eea95669fd1444e
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-09-09 15:36:20 +0000

    + BGUSO-144 Тестування анкети-додатку № 2 : Аркада

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tests/PdfWord/PdfReaderResearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BGU.DRPL.SignificantOwnership.Tests.PdfWord
+{
+    [TestFixture]
+    public class PdfReaderResearch
+    {
+        [Test]
+        public void Read()
+        { 
+            
+        }
+    }
+}
