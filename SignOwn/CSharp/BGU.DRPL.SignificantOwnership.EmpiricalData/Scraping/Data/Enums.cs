43fb0c9eb48930eafcdf2fe08add79d8956a394e
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-09-14 15:58:10 +0000

    + BGUSO-144 Тестування анкети-додатку № 2 : Аркада - in progress

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.EmpiricalData/Scraping/Data/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.EmpiricalData.Scraping.Data
+{
+    public enum Post328DodsRowType
+    { 
+        None = 0,
+        Dod2Principal,
+        Dod2Continuation,
+        Dod2Formula,
+        Dod2FormulaContinuation,
+        Dod3Principal,
+        Dod3Continuation
+    }
+}
