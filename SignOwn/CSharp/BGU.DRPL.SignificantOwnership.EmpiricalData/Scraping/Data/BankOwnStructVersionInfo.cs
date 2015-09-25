afab55d710a3dc41be0cebea9f597644968f8548
jefe <vmdrot@users.noreply.github.com>
2015-09-25 16:24:17 +0000

    +* BGUSO-145 ТЗ: роздрук структур даних для анкет - in progress + множинна резидентність

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.EmpiricalData/Scraping/Data/BankOwnStructVersionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.EmpiricalData.Scraping.Data
+{
+    public class BankOwnStructVersionInfo
+    {
+        public string MFO { get; set; }
+        public DateTime AsOf { get; set; }
+        public string Url { get; set; }
+        public long FileSize { get; set; }
+    }
+}
