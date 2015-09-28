bd523bc349d404cb1e2207a01a006332f00e57d7
jefe <vmdrot@users.noreply.github.com>
2015-09-28 15:30:53 +0000

    + BGUSO-147 Sensitive data anonymizer - started

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/Interfaces/IAnonymizeable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGU.DRPL.SignificantOwnership.Core.Interfaces
+{
+    public interface IAnonymizeable
+    {
+        void Anonymize(List<Type> involveTypes);
+    }
+}
