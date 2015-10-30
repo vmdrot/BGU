cc151fe63e64a1ca535978f457857a954d4a21d2
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-10-30 15:03:50 +0000

    +* BKRGSTR-19 DRAuto: Корекція змін - not without significant success, failed to open context menu :(

---
+++ BankInfo/DRClientAutomation/BGU.DRPL.DRClientAutomation/BGU.DRPL.DRClientAutomation.Console/ApplyBulkChangesSummaryCorrectionSingle.cmd
@@ -0,0 +1,5 @@
+SET PAUSE_MS=3000
+SET BEMUL=true
+SET MAX_CNT=0
+
+CALL ApplyBulkChangesSummaryCorrectionSingle_worker.cmd D:\home\vmdrot\BGU\Var\DerzhReiestr\OshchadBulkChange\RealPackages\BGU-DRPL_-_OshchadBulkChanges_20151029_PROD.orig.xml "%PAUSE_MS%" "%BEMUL%" "%MAX_CNT%" 311647 
