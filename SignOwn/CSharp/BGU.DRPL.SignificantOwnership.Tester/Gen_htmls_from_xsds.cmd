b3115d0b58c773f8d22fef6fced2797f09e21d6b
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2015-11-07 21:41:51 +0000

    + BGUSO-150 Анкетування: Досвід роботи: Врахування зауважень М.Бахтіярової +* re-generated HTML's from XSD's

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tester/Gen_htmls_from_xsds.cmd
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tester/Gen_htmls_from_xsds.cmd
@@ -4,3 +4,11 @@ CALL Gen_html_from_xsd_worker.cmd ..\..\XSDs\BankOwnershipStructureP328Message.f
 
 copy ..\..\XSDs\BankOwnershipStructureP328.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\BGU-DRPL_-_BankOwnershipStructureP328.full.uk-UA.xsd.html
 copy ..\..\XSDs\ActualQuestionnairesUnion.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\BGU-DRPL_-_Significant_ownership_structs_spec.v.1.0.html
+
+copy ..\..\XSDs\BankOwnershipStructureP328.full.uk-UA.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
+copy ..\..\XSDs\BankOwnershipStructureP328.uk-UA.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
+copy ..\..\XSDs\BankOwnershipStructureP328.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
+
+copy ..\..\XSDs\ActualQuestionnairesUnion.full.uk-UA.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
+copy ..\..\XSDs\ActualQuestionnairesUnion.uk-UA.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
+copy ..\..\XSDs\ActualQuestionnairesUnion.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
