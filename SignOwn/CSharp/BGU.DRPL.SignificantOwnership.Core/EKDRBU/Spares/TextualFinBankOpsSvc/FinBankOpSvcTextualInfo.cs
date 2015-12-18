2fc41d44b814217d99db6233fdaebd99b0ad0576
jefe <vmdrot@users.noreply.github.com>
2015-12-18 17:04:31 +0000

    +* BKRGSTR-20 Електронний еквівалент додатку 15 - started, in progress

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/EKDRBU/Spares/TextualFinBankOpsSvc/FinBankOpSvcTextualInfo.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/EKDRBU/Spares/TextualFinBankOpsSvc/FinBankOpSvcTextualInfo.cs
@@ -7,6 +7,13 @@ namespace BGU.DRPL.SignificantOwnership.Core.EKDRBU.Spares.TextualFinBankOpsSvc
 {
     public class FinBankOpSvcTextualInfo
     {
+        public FinBankOpSvcTextualInfo() { }
+        public FinBankOpSvcTextualInfo( string nrLtrBullet, string text)
+        {
+            this.NrLtrBullet = nrLtrBullet;
+            this.Text = text;
+        }
+        
         public string NrLtrBullet { get; set; }
         public string Text { get; set; }
         public List<FinBankOpSvcTextualInfo> PartialOpsSvcs { get; set; }
