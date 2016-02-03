89dde804f01af10897f478cf6ec610a2816584f9
jefe <vmdrot@users.noreply.github.com>
2016-02-03 16:05:14 +0000

    +* BGUSO-153 Пост.328: Зразок заповненої - in progress

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Facade/Anonymization/SensitiveDataAnonymizer.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Facade/Anonymization/SensitiveDataAnonymizer.cs
@@ -2,11 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BGU.DRPL.SignificantOwnership.Core.Questionnaires;
+using BGU.DRPL.SignificantOwnership.Core.Spares.Data;
 
 namespace BGU.DRPL.SignificantOwnership.Facade.Anonymizing
 {
     public class SensitiveDataAnonymizer
     {
+        private List<string> _usedPhysicalPersonCodes;
+        private List<string> _usedLegalPersonCodes;
+
+        public SensitiveDataAnonymizer()
+        {
+            _usedLegalPersonCodes = new List<string>();
+            _usedPhysicalPersonCodes = new List<string>();
+        }
+
+        public void AnonymizeCodes(IQuestionnaire subj)
+        {
+            //todo
+        }
+
+        public Dictionary<GenericPersonID, GenericPersonID> GeneratePersonIDs(List<GenericPersonID> haystack)
+        {
+            List<string> alreadyProcessed = new List<string>();
+            Dictionary<GenericPersonID, GenericPersonID> rslt = new Dictionary<GenericPersonID, GenericPersonID>();
+            foreach (GenericPersonID key in rslt.Keys)
+            {
+                if (alreadyProcessed.Contains(key.HashID))
+                    continue;
+
+            }
+
+            return rslt;
+        }
+
+        public string GeneratePersonCode(GenericPersonID oldId)
+        {
+            GenericPersonID rslt = new GenericPersonID() { CountryISO3Code = oldId.CountryISO3Code, DisplayName = oldId.DisplayName, PersonType = oldId.PersonType };
+            while (true)
+            { 
+
+            }
+        }
+
+        public string GeneratePhysicalPersonIPN()
+        {
+            return GenerateRandomStringWorker(10);
+        }
+
+        public string GenerateYeDRPOU()
+        {
+            return GenerateRandomStringWorker(8);
+        }
+
+        private string GenerateRandomStringWorker(int len)
+        {
+            Random r = new Random();
+            string rslt = string.Empty;
+            while (rslt.Length < len)
+            {
+                double dbl = r.NextDouble();
+                rslt += (dbl * 10000000000).ToString().Replace(".", string.Empty).Replace(",", string.Empty);
+            }
+            return rslt.Substring(0, len);
+
+        }
 
     }
 }
---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tests/QuestsMsgs/Post328Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using BGU.DRPL.SignificantOwnership.Core.Questionnaires;
+using BGU.DRPL.SignificantOwnership.Utility;
+using BGU.DRPL.SignificantOwnership.Core.Checks;
+
+namespace BGU.DRPL.SignificantOwnership.Tests.QuestsMsgs
+{
+    [TestFixture]
+    public class Post328Tests
+    {
+        [Test]
+        public void ConvertAppx2ToP328Msg_Arkada()
+        {
+            BankOwnershipStructureP328 rslt = new BankOwnershipStructureP328();
+            Appx2OwnershipStructLP appx2 = Tools.ReadXML<Appx2OwnershipStructLP>(@"D:\home\vmdrot\BGU\Specs\SignigicantOwnership\Testing\Arkada\ArkadaOwnershipChainParserTest_Appx2Qu.xml");
+            rslt.BankRef = appx2.BankRef;
+            rslt.MentionedIdentities = new List<Core.Spares.Data.GenericPersonInfo>();
+            rslt.MentionedIdentities.AddRange(appx2.MentionedIdentities);
+            rslt.ContactPerson = appx2.ContactPerson;
+            if (appx2.Signatory != null)
+                rslt.Signatory = appx2.Signatory;
+            else
+            {
+                rslt.Signatory = new Core.Spares.Data.SignatoryInfo();
+                rslt.Signatory.DateSigned = DateTime.Parse("2015-08-18T00:00:00");
+                rslt.Signatory.SurnameInitials = "Паливода К.В.";
+                rslt.Signatory.SignatoryPosition = "Голова Правління ПАТ АКБ \"АРКАДА\"";
+                rslt.Signatory.IsActingByPowOfAttorney = false;
+            }
+            if (appx2.Signatory != null && appx2.Signatory.DateSigned != null)
+                rslt.DateAsOf = (DateTime)appx2.Signatory.DateSigned;
+            else rslt.DateAsOf = DateTime.Parse("2015-08-18T00:00:00");
+            rslt.IsApplicationInfoAccurateAndTrue = true;
+            rslt.OwnershipsHive = new List<Core.Spares.Data.OwnershipStructure>();
+            rslt.OwnershipsHive.AddRange (appx2.BankExistingCommonImplicitOwners);
+            rslt.PersonsLinks = new List<Core.Spares.Data.PersonsAssociation>();
+            rslt.PersonsLinks.AddRange(appx2.PersonsLinks);
+            Appx2OwnershipStructLPChecker checker = new Appx2OwnershipStructLPChecker();
+            checker.Questionnaire = appx2;
+            rslt.UltimateOwners = new List<Core.Spares.Data.TotalOwnershipDetailsInfoEx>();
+            rslt.UltimateOwners.AddRange(checker.ListUltimateBeneficiaries(appx2.BankRef.LegalPerson));
+            Tools.WriteXML<BankOwnershipStructureP328>(rslt, @"D:\home\vmdrot\BGU\Specs\SignigicantOwnership\Testing\Arkada\ArkadaOwnershipChainParserTest_p328Msg.xml");
+        }
+    }
+}
