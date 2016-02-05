feb1d74cd81364c5af2561bf33ce44b5f94e1075
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2016-02-05 17:06:03 +0000

    + BGU-125 Оцифровка повідомлення про договір/зміни до договору кредиту від нерезидента - started

---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/FXLoansReg/Spares/AgreementAttributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace BGU.DRPL.SignificantOwnership.Core.FXLoansReg.Spares
+{
+    public class AgreementAttributes
+    {
+        [DisplayName("Назва")]
+        [Description("Назва документа - угода, договір, додаткова угода, додаток, протокол, тощо")]
+        public string Nm { get; set; }
+
+        [DisplayName("№")]
+        [Description("№ документа (угоди, договору, додаткової угоди, додатка, протоколу, тощо")]
+        public string Nr { get; set; }
+
+        [DisplayName("Дата")]
+        [Description("Дата документа (угоди, договору, додаткової угоди, додатка, протоколу, тощо")]
+        public DateTime Dt { get; set; }
+
+    }
+}
---
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Core/FXLoansReg/Spares/SWIFPaymentReqs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BGU.DRPL.SignificantOwnership.Core.Spares.Dict;
+
+namespace BGU.DRPL.SignificantOwnership.Core.FXLoansReg.Spares
+{
+    public class SWIFPaymentReqs
+    {
+        public string Field59Bene { get; set; }
+        public BankInfo Field57BeneBank { get; set; }
+        public List<BankInfo> CorrespondentBanks { get; set; }
+    }
+}
