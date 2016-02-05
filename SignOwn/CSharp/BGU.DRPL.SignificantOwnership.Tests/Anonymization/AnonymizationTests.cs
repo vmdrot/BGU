47d07fa67549375e47349e34e9ec5f4e5826e501
jefe <vmdrot@users.noreply.github.com>
2016-02-05 08:50:06 +0000

    +* BGUSO-154 Пост.328: XSLT шаблон для прикладів візуалізації - in progress+* BGUSO-147 Sensitive data anonymizer - in progress

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tests/Anonymization/AnonymizationTests.cs
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tests/Anonymization/AnonymizationTests.cs
@@ -9,19 +9,36 @@ using BGU.DRPL.SignificantOwnership.Utility;
 using BGU.DRPL.SignificantOwnership.Core.Spares.Data;
 using Newtonsoft.Json;
 using System.IO;
+using System.Xml;
 
 namespace BGU.DRPL.SignificantOwnership.Tests.Anonymization
 {
     [TestFixture]
     public class AnonymizationTests
     {
+        private static readonly string ANONYMIZED_p328_IN_TESTING_FOLDER = @"D:\home\vmdrot\BGU\Specs\SignigicantOwnership\Testing\Arkada\ArkadaOwnershipChainParserTest_p328Msg.xml";
+        private static readonly string XSLT_DEBUGGING_p328_PATH = @"D:\home\vmdrot\DEV\BGU\SignOwn\CSharp\BGU.DRPL.SignificantOwnership.XSLT\Samples\p328Msg_xsltEnabled.xml";
         [Test]
         public void AnonymizePost328Msg()
         {
-            BankOwnershipStructureP328 msg = Tools.ReadXML<BankOwnershipStructureP328>(@"D:\home\vmdrot\BGU\Specs\SignigicantOwnership\Testing\Arkada\ArkadaOwnershipChainParserTest_p328Msg.xml");
+            BankOwnershipStructureP328 msg = Tools.ReadXML<BankOwnershipStructureP328>(ANONYMIZED_p328_IN_TESTING_FOLDER);
             SensitiveDataAnonymizer anonymizer = new SensitiveDataAnonymizer();
             msg.Anonymize(anonymizer);
-            Tools.WriteXML<BankOwnershipStructureP328>(msg, @"D:\home\vmdrot\BGU\Specs\SignigicantOwnership\Testing\Arkada\ArkadaOwnershipChainParserTest_p328Msg_Anonymized.xml");
+            Tools.WriteXML<BankOwnershipStructureP328>(msg, ANONYMIZED_p328_IN_TESTING_FOLDER);
+            CopyAndInsertXsltRef();
+        }
+
+        [Test]
+        public void CopyAndInsertXsltRef()
+        {
+            #region add xslt ref
+            //<?xml-stylesheet type='text/xsl' href='../Templates/BankOwnershipStructureP328.xslt'?>
+            XmlDocument doc = new XmlDocument();
+            doc.Load(ANONYMIZED_p328_IN_TESTING_FOLDER);
+            XmlProcessingInstruction xslRef = doc.CreateProcessingInstruction("xml-stylesheet", "type='text/xsl' href='../Templates/BankOwnershipStructureP328.xslt'");
+            doc.InsertBefore((XmlNode)xslRef, doc.ChildNodes[1]);
+            #endregion
+            doc.Save(XSLT_DEBUGGING_p328_PATH);
         }
 
         [Test]
@@ -34,7 +51,7 @@ namespace BGU.DRPL.SignificantOwnership.Tests.Anonymization
 
 
             Dictionary<GenericPersonID, GenericPersonID> old2NewIds = anonymizer.GeneratePersonIDs(oldIds);
-            JsonSerializerSettings settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, NullValueHandling = NullValueHandling.Ignore };
+            JsonSerializerSettings settings = new JsonSerializerSettings() { Formatting = Newtonsoft.Json.Formatting.Indented, NullValueHandling = NullValueHandling.Ignore };
             File.WriteAllText(@"D:\home\vmdrot\BGU\Specs\SignigicantOwnership\Testing\Arkada\old2newIds.json", JsonConvert.SerializeObject(old2NewIds, settings), Encoding.Unicode);
 
         }
