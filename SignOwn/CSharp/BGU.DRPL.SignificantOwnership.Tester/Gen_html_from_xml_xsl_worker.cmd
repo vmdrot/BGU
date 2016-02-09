1e5229cf4f54bb35d7ce76d9e51afe0bf68bc760
jefe <vmdrot@users.noreply.github.com>
2016-02-09 16:10:12 +0000

    +* BGUSO-154 Пост.328: XSLT шаблон для прикладів візуалізації : генератор HTML: підчистка попередньо згерерованого файлу

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tester/Gen_html_from_xml_xsl_worker.cmd
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tester/Gen_html_from_xml_xsl_worker.cmd
@@ -1,4 +1,5 @@
-SET IN_XML_FILE=%1
-SET IN_XSL_FILE=%2
+SET IN_XML_FILE=%~1
+SET IN_XSL_FILE=%~2
 
-..\3dPartyLibraries\Microsoft\MSXsl\msxsl.exe %IN_XML_FILE% %IN_XSL_FILE% -o %IN_XML_FILE%.html > %IN_XML_FILE%.txt
+DEL "%IN_XML_FILE%.html"
+..\3dPartyLibraries\Microsoft\MSXsl\msxsl.exe "%IN_XML_FILE%" "%IN_XSL_FILE%" -o "%IN_XML_FILE%.html"  >"%IN_XML_FILE%.txt"
