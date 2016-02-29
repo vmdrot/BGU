8f044c1393be4802f9535a97597b6816ff16eb3f
jefe <jefe@fd8a697f-def4-475f-9a9f-d0d55303892f>
2016-02-29 12:36:23 +0000

    +* BGUSO-154 Пост.328: XSLT шаблон для прикладів візуалізації : clean-up, finishing touches

--- SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tester/Gen_htmls_from_xsds.cmd
+++ SignOwn/CSharp/BGU.DRPL.SignificantOwnership.Tester/Gen_htmls_from_xsds.cmd
@@ -3,11 +3,13 @@ CALL Gen_html_from_xsd_worker.cmd ..\..\XSDs\BankOwnershipStructureP328.full.uk-
 CALL Gen_html_from_xsd_worker.cmd ..\..\XSDs\BankOwnershipStructureP328Message.full.uk-UA.xsd
 REM CALL Gen_html_from_xsd_worker.cmd ..\..\XSDs\StateBankBranchRegistryChangePackageV1.full.uk-UA.xsd
 CALL Gen_html_from_xsd_worker.cmd ..\..\XSDs\ActualEKDRBUStructsUnion.full.uk-UA.xsd
+CALL Gen_html_from_xsd_worker.cmd ..\..\XSDs\FinBankLicensingEnumsUnion.full.uk-UA.xsd
 
 copy ..\..\XSDs\BankOwnershipStructureP328.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\BGU-DRPL_-_BankOwnershipStructureP328.full.uk-UA.xsd.html
 copy ..\..\XSDs\ActualQuestionnairesUnion.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\BGU-DRPL_-_Significant_ownership_structs_spec.v.1.0.html
 REM copy ..\..\XSDs\StateBankBranchRegistryChangePackageV1.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Var\DerzhReiestr\ShBO\TORs\BGU-DRPL_-_BankInfo_structs_spec.v.1.0.html
 copy ..\..\XSDs\ActualEKDRBUStructsUnion.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Var\DerzhReiestr\ShBO\TORs\BGU-DRPL_-_BankInfo_structs_spec.v.1.0.html
+copy ..\..\XSDs\FinBankLicensingEnumsUnion.full.uk-UA.xsd.html D:\home\vmdrot\BGU\Var\FXRetailers\TORs\FinBankLicensingEnumsUnion.full.uk-UA.xsd.html
 
 
 copy ..\..\XSDs\BankOwnershipStructureP328.full.uk-UA.xsd D:\home\vmdrot\BGU\Specs\SignigicantOwnership\TORs\
@@ -25,3 +27,7 @@ REM copy ..\..\XSDs\StateBankBranchRegistryChangePackageV1.xsd D:\home\vmdrot\BG
 copy ..\..\XSDs\ActualEKDRBUStructsUnion.full.uk-UA.xsd D:\home\vmdrot\BGU\Var\DerzhReiestr\ShBO\TORs\
 copy ..\..\XSDs\ActualEKDRBUStructsUnion.uk-UA.xsd D:\home\vmdrot\BGU\Var\DerzhReiestr\ShBO\TORs\
 copy ..\..\XSDs\ActualEKDRBUStructsUnion.xsd D:\home\vmdrot\BGU\Var\DerzhReiestr\ShBO\TORs\
+
+copy ..\..\XSDs\FinBankLicensingEnumsUnion.full.uk-UA.xsd D:\home\vmdrot\BGU\Var\FXRetailers\TORs
+copy ..\..\XSDs\FinBankLicensingEnumsUnion.uk-UA.xsd D:\home\vmdrot\BGU\Var\FXRetailers\TORs
+copy ..\..\XSDs\FinBankLicensingEnumsUnion.xsd D:\home\vmdrot\BGU\Var\FXRetailers\TORs
