b6082a1cb5e69eab097403c4f79b111367dcba0f
vmdrot <valeriy.drotenko@gmail.com>
2019-05-29 15:30:11 +0300

    PDF reading: research #4: an alternative branch - Sautinsoft.PdfFocus varian: works perfectly, but randomly puts 'trial' here and there, costs ~ $400

---
+++ PdfReadResearch/Pdf2Docx/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdf2Docx
+{
+    /// <summary>
+    /// This blessing treasure we awe to https://code.msdn.microsoft.com/How-to-convert-PDF-to-DOCX-fc9e7186/
+    /// </summary>
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return Pdf2Docx(args);
+        }
+
+        public static int Pdf2Docx(string[] args)
+        {
+            string pdfFile = args[0];
+            string wordFile = args[1];
+
+            // Convert PDF file to DOCX file 
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+
+            f.OpenPdf(pdfFile);
+
+            if (f.PageCount == 0)
+                return 0;
+            // You may choose output format between Docx and Rtf. 
+            f.WordOptions.Format = SautinSoft.PdfFocus.CWordOptions.eWordDocument.Docx;
+
+            int result = f.ToWord(wordFile);
+            
+            if (result == 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
